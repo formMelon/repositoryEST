c97994a067515e7ef5b7622139d7427246e3c28f
20231162 <o.p.moloney@tue.student.nl>
2024-06-10 20:05:19 +0200

    Almost done.

--- EST-model-main/preprocessing.m
+++ EST-model-main/preprocessing.m
@@ -1,64 +1,67 @@
 % Pre-processing script for the EST Simulink model. This script is invoked
-% before the Simulink model starts running (initFcn callback function).
+% before the Simulink model starts running. (initFcn callback function.)
 
-%% Load the supply and demand data
+%% Load the supply and demand data.
 
 timeUnit   = 's';
 
 supplyFile = "Team62_supply.csv";
 supplyUnit = "kW";
 
-% load the supply data
+% Load the supply data...
 Supply = loadSupplyData(supplyFile, timeUnit, supplyUnit);
 
 demandFile = "Team62_demand.csv";
 demandUnit = "kW";
 
-% load the demand data
+% Load the demand data...
 Demand = loadDemandData(demandFile, timeUnit, demandUnit);
 
-%% Simulation settings
+%% Simulation settings.
 
 deltat = 5*unit("min");
 stopt  = min([Supply.Timeinfo.End, Demand.Timeinfo.End]);
 
-%% System parameters
+%% System parameters.
 
-% transport from supply
-resistance = 0.00002373556*unit("ohm"); % Resistance in the wires from 
-% solar panel panels to the inverter.
-aInverter = 0.03; % Coefficient for loss from solar inverter, ranges from 
-% 0.05 to 0.03
-efficiencyTransformer = 0.99; % For step-ups and step-downs, ranges from 
-% 0.97 to 0.99
+% Transport from supply.
+stringVoltage = 408*unit("V"); % Considering a constant working voltage of 
+% 24 V and strings of 17 solar panels. Per string that is a voltage of 408 
+% V.
+resistance = 0.00002373556*unit("ohm"); % Resistance in the wires from the
+% solar panels to the inverter. Assuming a wire length of 5 m, a diameter
+% of 8 mm, and copper as a conducter.
+efficiencyInverter = 0.97; % Efficiency for solar inverter, ranges from
+% 0.95 to 0.97.
 
+% Injection system.
+aNetInjection = 0.05; % Dissipation coefficient during injection.
 
-% injection system
-aInjection = 0.05; % Dissipation coefficient during injection
-
-% Storage system
-
-EStorageMax     = 350000*unit("kWh"); % Maximum energy for energy storage
-EStorageMin     = 0.0*unit("kWh"); % Minimum energy
-EStorageInitial = 0.0*unit("kWh"); % Initial energy
-bStorage        = 1e-8/unit("s");  % Energy storage dissipation coefficient
+% Storage system.
+EStorageMax     = 450000*unit("kWh"); % Maximum energy for energy storage.
+EStorageMin     = 0.0*unit("kWh"); % Minimum energy.
+EStorageInitial = 0.0*unit("kWh"); % Initial energy.
+bStorage        = 1e-8/unit("s");  % Energy storage dissipation 
+% coefficient.
 sb = 5.670374419*10^-8*(...
     unit("W")/unit("m")/unit("m")/unit("K")/unit("K")/unit("K")/unit("K")); 
-% Constant of Stefan
-mass = 40500000000*unit("kg"); % Mass of the thermal storage assuming the 
-% use of water
-k = 36*unit("W")/unit("m")/unit("K"); % The thermal conductivity for the 
-% steel tanks
-areaOfTank = 4100; % Area of the tanks, likely larger than suggested here
+% Constant of Stefan.
+mass = 25837320*unit("kg"); % Mass of the thermal storage assuming the 
+% use of water.
+k = 0.04*unit("W")/unit("m")/unit("K"); % The thermal conductivity for the 
+% insulation, syrafoam in this case.
+areaOfTank = 6096*unit("m")*unit("m"); % Area of the thermal storage tank.
 environmentalTemperature = 293*unit("K"); % Assumed to be constant but 
-% could fluctuate between 5 and 23 degrees celcius.
-x = 0.1*unit("m"); % Thickness of tank
-e = 0.07; % Emmissivity of the steel of the tanks, could vary largely 
-% depending on the type of steel
+% could fluctuate between 5 and 23 degrees celcius in reality.
+x = 1*unit("m"); % Thickness of insulation.
+e = 0.07; % Emmissivity of the steel of the thermal storage tank, could 
+% vary largely depending on the type of steel.
 
-% extraction system
-aExtraction = 0.15; % Dissipation coefficient including generator losses.
+% Extraction system.
+aExtraction = 0.11; % Dissipation coefficient including generator losses.
 
-% transport to demand
+% Transport to demand.
 aDemandTransport = 0.02; % Dissipation coefficient during high voltage 
-% transport from the Flevopolder to distribution in Amsterdam
+% transport from the Flevopolder to distribution in Amsterdam.
+efficiencyTransformer = 0.99; % Efficiency for step-ups and step-downs,
+% ranges from 0.97 to 0.99.
--- EST-model-main/scripts/constants.m
+++ EST-model-main/scripts/constants.m
@@ -3,12 +3,15 @@
 global unit;
 unit = containers.Map;
 
-% time
+
+% Time.
+
 unit("s")    = 1.;
 unit("min")  = 60*unit("s");
 unit("h")    = 60*unit("min");
 unit("day")  = 24*unit("h");
 unit("year") = 365*unit("day");
+
 
 % energy
 unit("J")  = 1.;
@@ -16,27 +19,46 @@ unit("kJ") = 1000*unit("J");
 unit("MJ") = 1000*unit("kJ");
 unit("GJ") = 1000*unit("MJ");
 
-% power
+
+% Power
 unit("W")  = unit("J")/unit("s");
 unit("kW") = 1000*unit("W");
 unit("MW") = 1000*unit("kW");
 unit("GW") = 1000*unit("MW");
 
+
 % energy (Wh)
-unit("Wh")  = unit("W") *unit("h");
+unit("Wh")  = unit("W")*unit("h");
 unit("kWh") = unit("kW")*unit("h");
 unit("MWh") = unit("MW")*unit("h");
 unit("GWh") = unit("GW")*unit("h");
+
 
 % Resistance
 
 unit("ohm") = 1.;
 
-% Temperature
+
+% Temperature.
 
 unit("K") = 1.;
 
-% Weight
+
+% Mass.
 
 unit("kg") = 1.;
-unit("m") = 1.
+
+
+% Length.
+
+unit("m") = 1.;
+
+
+% Potential.
+
+unit("V") = 1.;
+
+
+% Current.
+
+unit("A") = 1.;
